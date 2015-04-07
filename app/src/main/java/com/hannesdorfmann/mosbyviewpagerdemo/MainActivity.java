a623f4f1ff0acf06751264ac2ba17f312797804b
Rakesh Waghela <javaindia@gmail.com>
2015-04-07 20:04:45 +0530

    Improved the left/right swipe navigation in pager
    It seem to be a common cause from swiperefreshlayout to act on touch events without letting in it's children's handle the movements properly.
    
    Following solution was handy to remove that touch handling for left right swiping.
    http://stackoverflow.com/a/27934705/861454

--- app/src/main/java/com/hannesdorfmann/mosbyviewpagerdemo/MainActivity.java
+++ app/src/main/java/com/hannesdorfmann/mosbyviewpagerdemo/MainActivity.java
@@ -3,74 +3,107 @@ package com.hannesdorfmann.mosbyviewpagerdemo;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.view.MotionEvent;
+import android.view.View;
+
 import butterknife.InjectView;
+
 import com.hannesdorfmann.mosby.dagger1.viewstate.lce.Dagger1MvpLceViewStateActivity;
 import com.hannesdorfmann.mosby.mvp.viewstate.lce.ParcelableLceViewState;
 import com.hannesdorfmann.mosby.mvp.viewstate.lce.data.ParcelableDataLceViewState;
 import com.hannesdorfmann.mosbyviewpagerdemo.model.PostList;
 
 public class MainActivity
-    extends Dagger1MvpLceViewStateActivity<SwipeRefreshLayout, PostList, PostsView, PostsPresenter>
-    implements PostsView, SwipeRefreshLayout.OnRefreshListener {
+        extends Dagger1MvpLceViewStateActivity<SwipeRefreshLayout, PostList, PostsView, PostsPresenter>
+        implements PostsView, SwipeRefreshLayout.OnRefreshListener {
 
-  @InjectView(R.id.viewPager) ViewPager viewPager;
+    @InjectView(R.id.viewPager)
+    ViewPager viewPager;
 
-  PostPagerAdapter adapter;
+    PostPagerAdapter adapter;
 
-  @Override protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-    contentView.setOnRefreshListener(this);
-  }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        contentView.setOnRefreshListener(this);
+        viewPager.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
 
-  @Override public ParcelableLceViewState<PostList, PostsView> createViewState() {
-    return new ParcelableDataLceViewState<>();
-  }
-
-  @Override public PostList getData() {
-    return adapter.getPostList();
-  }
-
-  @Override protected String getErrorMessage(Throwable throwable, boolean b) {
-    return "An error has occurred";
-  }
-
-  @Override protected PostsPresenter createPresenter() {
-    return getObjectGraph().get(PostsPresenter.class);
-  }
-
-  @Override public void setData(PostList postList) {
-    adapter = new PostPagerAdapter(getSupportFragmentManager());
-    adapter.setPostList(postList);
-    viewPager.setAdapter(adapter);
-  }
-
-  @Override public void loadData(boolean b) {
-    presenter.loadPosts(b);
-  }
-
-  @Override public void onRefresh() {
-    loadData(true);
-  }
-
-  @Override public void showError(Throwable e, boolean pullToRefresh) {
-    super.showError(e, pullToRefresh);
-    contentView.setRefreshing(false);
-  }
-
-  @Override public void showContent() {
-    super.showContent();
-    contentView.setRefreshing(false);
-  }
-
-  @Override public void showLoading(boolean pullToRefresh) {
-    super.showLoading(pullToRefresh);
-    if (!contentView.isRefreshing()){
-      contentView.post(new Runnable() {
-        @Override public void run() {
-          contentView.setRefreshing(true);
-        }
-      });
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_MOVE:
+                        contentView.setEnabled(false);
+                        break;
+                    case MotionEvent.ACTION_UP:
+                    case MotionEvent.ACTION_CANCEL:
+                        contentView.setEnabled(true);
+                        break;
+                }
+                return false;
+            }
+        });
     }
-  }
+
+    @Override
+    public ParcelableLceViewState<PostList, PostsView> createViewState() {
+        return new ParcelableDataLceViewState<>();
+    }
+
+    @Override
+    public PostList getData() {
+        return adapter.getPostList();
+    }
+
+    @Override
+    protected String getErrorMessage(Throwable throwable, boolean b) {
+        return "An error has occurred";
+    }
+
+    @Override
+    protected PostsPresenter createPresenter() {
+        return getObjectGraph().get(PostsPresenter.class);
+    }
+
+    @Override
+    public void setData(PostList postList) {
+        adapter = new PostPagerAdapter(getSupportFragmentManager());
+        adapter.setPostList(postList);
+        viewPager.setAdapter(adapter);
+    }
+
+    @Override
+    public void loadData(boolean b) {
+        presenter.loadPosts(b);
+    }
+
+    @Override
+    public void onRefresh() {
+        loadData(true);
+    }
+
+    @Override
+    public void showError(Throwable e, boolean pullToRefresh) {
+        super.showError(e, pullToRefresh);
+        contentView.setRefreshing(false);
+    }
+
+    @Override
+    public void showContent() {
+        super.showContent();
+        contentView.setRefreshing(false);
+    }
+
+    @Override
+    public void showLoading(boolean pullToRefresh) {
+        super.showLoading(pullToRefresh);
+        if (!contentView.isRefreshing()) {
+            contentView.post(new Runnable() {
+                @Override
+                public void run() {
+                    contentView.setRefreshing(true);
+                }
+            });
+        }
+    }
 }
